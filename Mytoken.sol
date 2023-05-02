24c033f526066a96f7da5246b447f690cbc95bbd
AlberAquino <129314028+AlberAquino@users.noreply.github.com>
2023-05-02 22:00:44 +0800

    Update Mytoken.sol

--- Mytoken.sol
+++ Mytoken.sol
@@ -26,21 +26,3 @@ uint public  TotalSupply = 0;
        
     
 
-Click the "Solidity Compiler" tab in the left-hand sidebar to compile the code. Click the "Compile myToken.sol" button after making sure the "Compiler" option is selected to "0.8.18" (or another compatible version).
-
-Select "Deploy and Run Transactions" to start the contract deployment process. You can deploy the contract from a new window that will open as a result. Before deploying, don't forget to choose the "MyToken" contract.
-
-Set the parameters for the balance, mint, and burn functions in the "Deployed Contracts" section of the deployment window.
-
-Enter the recipient's address and the desired quantity of new tokens, then click "transact" to start the token minting process.
-Enter the recipient's address and the desired number of tokens, then click "transact" to burn them.
-Enter the recipient's address and the desired token production quantity, then click call to view the address's current balances. By selecting the "totalSupply" button, you can also view the total supply.
-# Authors
-NTCIAN Alber C Aquino
-Discord: @albercaquino
-#4427
-
-# license
-MIT
-Remix - Ethereum IDE
-Sent
